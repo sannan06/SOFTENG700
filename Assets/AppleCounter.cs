3f6affbc8b7cc7dc1fe698b18bb941fec7ef9580
Alan.Thomas <alanunny@outlook.com>
2021-06-30 20:28:14 +1200

    added apple counter

---
+++ Assets/AppleCounter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleCounter : MonoBehaviour
+{
+    int totalApples;
+    int greenApples;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {  
+        while(true) {
+            Debug.Log($"{greenApples}/{totalApples}");
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        totalApples++;
+        if(other.tag.Equals("GreenApple")) {
+            greenApples++;
+        }
+
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        totalApples--;
+        if(other.tag.Equals("GreenApple")) {
+            greenApples--;
+        }
+
+    }
+}
